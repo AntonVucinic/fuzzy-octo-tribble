cb1621aa1077d01dfdb0ca47f7e1df768ba75720
Anton Vučinić <64925492+AntonVucinic@users.noreply.github.com>
2024-12-14 16:19:33 +0100

    Implement final endpoint

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,11 +1,11 @@
 from __future__ import annotations
 
 from typing import Annotated, Any, Literal
-from fastapi import FastAPI, File, UploadFile
+from fastapi import Body, FastAPI, UploadFile
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
 import google.generativeai as genai
-import tempfile
+import json
 
 class VatRate(BaseModel):
     rate: float
@@ -19,7 +19,7 @@ class Registration(BaseModel):
 
 class Deadline(BaseModel):
     filing_deadline: str #Fonoa has string
-    payment_deadline: str #Fonoa has string
+    payment_deadline: str | None #Fonoa has string
 
 
 class Return(BaseModel):
@@ -52,12 +52,11 @@ class Schema(BaseModel):
     exemptions: list[str]
     registration_requirements: Registration
     # filing_and_compliance: Filing Inside returns
-    returns:Return
+    returns:Return | None
     cross_border_transactions: CrossBorder
     deadlines: Deadline
     invoice: list[str]
     penalties: str
-
 
 
 app = FastAPI()
@@ -74,24 +73,26 @@ model_config = genai.GenerationConfig(
 
 genai.configure(api_key=settings.api_key)
 
-@app.get("/tax-info/text", response_model=Schema)
-async def upload_text(law: str) -> Any:
+@app.post("/tax-info/text")
+async def upload_text(law: Annotated[str, Body(media_type="text/plain")]) -> Any:
     prompt = f"""A user has uploaded the text of this countries tax law.
+    This law will most most likely be written in the nations official language so it's crucial to adapt and not look for literal symobls in text.
     Summarize this law using the provided JSON schema.
+
+    ---
 
     {law}
     """
     response = model.generate_content(prompt, generation_config=model_config)
-    return response['candidates'][0]['content']['parts'][0]['text']  # pyright: ignore[reportIndexIssue]
+    return json.loads(response.to_dict()['candidates'][0]['content']['parts'][0]['text'])  # pyright: ignore[reportIndexIssue]
 
 
-@app.post("/tax-info/file")
+@app.post("/tax-info/file", response_model=Schema)
 async def upload_file(law: UploadFile) -> Any:
-    file = genai.upload_file(law.file, mime_type="application/pdf")
+    file = genai.upload_file(law.file, mime_type="application/pdf") # pyright: ignore[reportArgumentType]
     prompt = f"""A user has uploaded a PDF file of this countries tax law.
     This law will most most likely be written in the nations official language so it's crucial to adapt and not look for literal symobls in text.
-    VAT is spelled PDV in Canada.
     Summarize this law using the provided JSON schema.
     """
     response = model.generate_content([prompt, file], generation_config=model_config)
-    return response.to_dict()['candidates'][0]['content']['parts'][0]['text']# pyright: ignore[reportIndexIssue]
+    return json.loads(response.to_dict()['candidates'][0]['content']['parts'][0]['text'])  # pyright: ignore[reportIndexIssue]
